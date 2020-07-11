e91d519f24463c53c0b16c6d968ac1cd25841b03
F. Talha Altınel <talhaaltinel@hotmail.com>
2020-07-11 23:06:31 +0100

    url config

--- easyfood_ecommerce/store/views.py
+++ easyfood_ecommerce/store/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+def store(request):
+    context = {}
+    render(request, "store/store.html", context)
+
+def checkout(request):
+    context = {}
+    render(request, "store/checkout.html", context)
+
+def cart(request):
+    context = {}
+    render(request, "store/cart.html", context)
+
