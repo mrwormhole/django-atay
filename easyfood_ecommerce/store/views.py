4399ee50acd2def1f5d4005a798e71537e237b28
F. Talha Altınel <talhaaltinel@hotmail.com>
2020-07-12 23:26:38 +0100

    sass setup

--- easyfood_ecommerce/store/views.py
+++ easyfood_ecommerce/store/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 
 def store(request):
     context = {}
-    render(request, "store/store.html", context)
+    return render(request, "store/store.html", context)
 
 def checkout(request):
     context = {}
-    render(request, "store/checkout.html", context)
+    return render(request, "store/checkout.html", context)
 
 def cart(request):
     context = {}
-    render(request, "store/cart.html", context)
+    return render(request, "store/cart.html", context)
 
