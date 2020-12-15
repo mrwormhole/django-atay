faca4a19d025edc6ec3a9d3c9485c93aab2460dd
F. Talha Altınel <talhaaltinel@hotmail.com>
2020-12-15 23:58:05 +0000

    templates

--- atay_trade/store/urls.py
+++ atay_trade/store/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.store, name="store"),
-    path("cart/", views.cart, name="cart"),
     path("checkout/", views.checkout, name="checkout"),
+    path("contact/", views.contact, name="contact"),
+    path("catalog/", views.catalog, name="catalog"),
+    path("product/", views.product, name="product")
 ]
--- atay_trade/store/views.py
+++ atay_trade/store/views.py
@@ -2,12 +2,20 @@ from django.shortcuts import render
 
 def store(request):
     context = {}
-    return render(request, "store/store.html", context)
+    return render(request, "store/index.html", context)
 
 def checkout(request):
     context = {}
     return render(request, "store/checkout.html", context)
 
-def cart(request):
+def contact(request):
     context = {}
-    return render(request, "store/cart.html", context)
+    return render(request, "store/contact.html", context)
+
+def catalog(request):
+    context = {}
+    return render(request, "store/catalog.html", context)
+
+def product(request):
+    context = {}
+    return render(request, "store/product.html", context)
