170020eab1bf8ddf788f2aeedbeace107df7ce48
F. Talha Altınel <talhaaltinel@hotmail.com>
2021-02-25 00:54:01 +0000

    cors policy

--- atay_trade/atay_trade/settings.py
+++ atay_trade/atay_trade/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.postgres',
+    'corsheaders',
     'rest_framework',
     'imagekit',
     'crispy_forms',
@@ -53,6 +54,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -60,6 +62,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware'
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "https://ataytrade.co.uk",
+    "https://www.ataytrade.co.uk",
 ]
 
 ROOT_URLCONF = 'atay_trade.urls'
